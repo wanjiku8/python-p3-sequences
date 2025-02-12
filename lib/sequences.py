3d5b03a975a11d9097e8fda14f324ce533d2bf4f
wanjiku8 <wanjikuf141@gmail.com>
2025-02-12 11:15:12 -0800

    comitt

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,11 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fibonacci = [0, 1]
+    
+    while len(fibonacci) < length:
+        fibonacci.append(fibonacci[-1] + fibonacci[-2])
+    
+    print(fibonacci[:length])
+
+print_fibonacci(9)
